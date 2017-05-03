ecc0ee66403aa3d1242416167505731624e29744
Rolando Santamaria Maso <kyberneees@gmail.com>
2017-05-03 13:37:52 +0200

    considering env for service hostname

--- config/default.js
+++ config/default.js
@@ -3,7 +3,7 @@ module.exports = {
         hydra: {
             'serviceName': process.env.SERVICE_NAME || 'default-service',
             'serviceDescription': 'Basic restana service on top of Hydra',
-            'serviceIP': '127.0.0.1',
+            'serviceIP': process.env.HOSTNAME || '127.0.0.1',
             'servicePort': process.env.PORT || 3000,
             'serviceType': 'restana',
             'serviceVersion': '1.0.0',
